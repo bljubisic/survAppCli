0885a43baf2db01ea8de0c5de5e06f99efa07545
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2023-11-17 20:03:54 +0100

    Added md.js to pars the markdown survey

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import md from '../md.js';
+
+		const text = "How old are you?\n ? age_group\n - 0-18\n - 19-35\n - 36-55\n - 56 or older";
+		const body = md(text);
+		console.log(body);
+</script>
